6a47316b5b17bd31d5e019fb655e989220f0099f
BestKora <developer@bestkora.com>
2018-11-24 18:10:11 +0300

    Initial Commit

---
+++ ImageGallery/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ImageGallery
+//
+//  Created by Tatiana Kornilova on 24/11/2018.
+//  Copyright © 2018 BestKora. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
